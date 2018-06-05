71c0972e04c520e523e703e147771e493494dea7
wangtao <1621829316@qq.com>
2018-06-05 14:07:21 +0800

    实现了一开始就加载歌曲列表

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -22,6 +22,15 @@
   let model = {
     data: {
       songs: []
+    },
+    fetch(){
+      var query = new AV.Query('Song');
+      return query.find().then((songs) => {
+        
+        this.data.songs = songs.map((song) => {
+          return {id: song.id, ...song.attributes}
+        })
+      })
     }
   }
 
@@ -29,12 +38,16 @@
     init(view, model){
       this.view = view
       this.model = model
+      //将ul渲染进模板
       this.view.render(this.model.data)
       window.eventHub.on('upload', () => {
         this.view.clearActive()
       })
       window.eventHub.on('create', (songData) => {
         this.model.data.songs.push(songData)
+        this.view.render(this.model.data)
+      })
+      this.model.fetch().then(() => {
         this.view.render(this.model.data)
       })
     }
